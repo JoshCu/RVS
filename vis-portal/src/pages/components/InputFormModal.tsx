243111f977d03f1c0af99087c8f0bbeba62e87cf
dmrudy17 <dmr136@zips.uakron.edu>
2023-06-27 09:00:31 -0400

    chore: move input form modal control up to parent index

--- vis-portal/src/pages/components/InputFormModal.tsx
+++ vis-portal/src/pages/components/InputFormModal.tsx
@@ -1,6 +1,10 @@
-import { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 
-const InputFormModal = () => {
+interface InputFormProps {
+  closeModal: () => void;
+}
+
+const InputFormModal: React.FC<InputFormProps> = ({ closeModal }) => {
   const [showModal, setShowModal] = useState(false);
   const [showEmailConfirmation, setShowEmailConfirmation] = useState(false);
   const [email, setEmail] = useState("");
@@ -68,63 +72,59 @@ const InputFormModal = () => {
     setEmail("");
     setIsValidEmail(true);
     setShowEmailConfirmation(false);
-    setShowModal(false);
+    closeModal();
   };
 
   return (
     <>
-      <button className="absolute top-3 right-2 py-2 px-4 bg-indigo-500 rounded text-white hover:bg-indigo-600" onClick={() => setShowModal(true)}>Submit New Game</button>
-      {showModal ? (
-        <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 shadow-lg">
-            <div className="w-96 min-w-96 h-60 min-h-60 bg-white rounded">
-              <div className="flex flex-col h-full">
-                {showEmailConfirmation ? (
-                  <div className="p-2 h-full w-full">
-                    <div className="h-full w-full">
-                      <p className="text-lg text-center h-4/5 w-full">A confirmation email has been sent to {email}, please follow the instructions there to continue.</p>
-                      <div className="h-1/5 w-full flex items-center justify-center">
-                        <button onClick={handleCloseModal} className="text-red-500 background-transparent font-bold uppercase px-6 py-1 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
-                          Close
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                ) : (
-                  <>
-                    <div className="p-2">
-                      <p className="text-3xl text-center">Request Creator Key</p>
-                      <form className="mt-4 flex-grow">
-                        <div className="mb-2">
-                          <label htmlFor="email" className="block text-sm font-bold">
-                            Email
-                          </label>
-                          <input id="email" onChange={handleEmailChange} className={`w-full py-1 border ${isValidEmail ? "border-gray-300" : "border-red-500"} border-gray-300 rounded focus:outline-none`} type="email" placeholder="user@domain.com" />
-                        </div>
-                      </form>
-                    </div>
-                    <div className="flex justify-end p-2 space-x-2">
-                      <button onClick={handleCloseModal} className="h-auto text-red-500 background-transparent font-bold uppercase px-6 py-1 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+      <>
+        <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 shadow-lg">
+          <div className="w-96 min-w-96 h-60 min-h-60 bg-white rounded">
+            <div className="flex flex-col h-full">
+              {showEmailConfirmation ? (
+                <div className="p-2 h-full w-full">
+                  <div className="h-full w-full">
+                    <p className="text-lg text-center h-4/5 w-full">A confirmation email has been sent to {email}, please follow the instructions there to continue.</p>
+                    <div className="h-1/5 w-full flex items-center justify-center">
+                      <button onClick={handleCloseModal} className="text-red-500 background-transparent font-bold uppercase px-6 py-1 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
                         Close
                       </button>
-                      <button
-                        onClick={handleFormSubmission}
-                        className="bg-indigo-500 text-white font-bold uppercase text-sm px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 disabled:bg-gray-400"
-                        disabled={isSubmitDisabled}
-                      >
-                        Submit
-                      </button>
                     </div>
-                  </>
-                )
-              }
-              </div>
+                  </div>
+                </div>
+              ) : (
+                <>
+                  <div className="p-2">
+                    <p className="text-3xl text-center">Request Creator Key</p>
+                    <form className="mt-4 flex-grow">
+                      <div className="mb-2">
+                        <label htmlFor="email" className="block text-sm font-bold">
+                          Email
+                        </label>
+                        <input id="email" onChange={handleEmailChange} className={`w-full py-1 border ${isValidEmail ? "border-gray-300" : "border-red-500"} border-gray-300 rounded focus:outline-none`} type="email" placeholder="user@domain.com" />
+                      </div>
+                    </form>
+                  </div>
+                  <div className="flex justify-end p-2 space-x-2">
+                    <button onClick={handleCloseModal} className="h-auto text-red-500 background-transparent font-bold uppercase px-6 py-1 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+                      Close
+                    </button>
+                    <button
+                      onClick={handleFormSubmission}
+                      className="bg-indigo-500 text-white font-bold uppercase text-sm px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 disabled:bg-gray-400"
+                      disabled={isSubmitDisabled}
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </>
+              )
+            }
             </div>
           </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
-      ) : null
-      }
+        </div>
+        <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+      </>
     </>
   )
 };
--- vis-portal/src/pages/index.tsx
+++ vis-portal/src/pages/index.tsx
@@ -2,10 +2,12 @@ import {Open_Sans} from 'next/font/google';
 import Head from 'next/head';
 import Form from './components/Form';
 import InputFormModal from './components/InputFormModal';
+import React, { useState } from 'react';
 
 const openSans = Open_Sans({subsets: ['latin']})
 
 export default function Home() {
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <>
@@ -13,8 +15,13 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <div className="h-screen w-screen overflow-hidden">
-          <h1 className="text-center text-3xl">VisPortal</h1>
-          <InputFormModal />
+          <div className="grid grid-cols-3">
+            <h1 className="m-auto text-center text-3xl col-start-2">VisPortal</h1>
+            <div className="w-full flex justify-end bg-blue-200">
+              <button className="mr-1 mt-1 py-2 px-4 bg-indigo-500 rounded text-white hover:bg-indigo-600" onClick={() => setShowModal(true)}>Submit New Game</button>
+            </div>
+          </div>
+          {showModal && <InputFormModal closeModal={() => setShowModal(false)} />}
           <div className="w-full h-full flex justify-center">
             <div id="main-card" className="bg-gray-200 w-3/4 h-3/4 m-auto shadow-lg flex flex-row">
               <Form />
