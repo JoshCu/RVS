8e1188df0d34c976feb5d0fea0c5a3972d97dd80
dmrudy17 <dmr136@zips.uakron.edu>
2023-06-29 12:19:40 -0400

    feat: add more progress on SubmitGameModal

--- vis-portal/src/pages/components/SubmitGameModal.tsx
+++ vis-portal/src/pages/components/SubmitGameModal.tsx
@@ -9,11 +9,6 @@ interface InputFormProps {
   closeModal: () => void;
 }
 
-type Requirement = {
-  field: string;
-  type: string;
-};
-
 const SubmitGameModal: React.FC<InputFormProps> = ({ closeModal }) => {
   const [email, setEmail] = useState("");
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
@@ -24,9 +19,12 @@ const SubmitGameModal: React.FC<InputFormProps> = ({ closeModal }) => {
   const [step, setStep] = useState(1);
   const [allFieldsFilled, setAllFieldsFilled] = useState(false);
   const [requirements, setRequirements] = useState<{ [key: string]: string }>({});
-  const [gameCreationError, setGameCreationError] = useState(true);
+  const [gameCreationError, setGameCreationError] = useState(false);
   const [gameCreationMessage, setGameCreationMessage] = useState('');
   const [gameCreationStatus, setGameCreationStatus] = useState(0);
+  const [scoreSubmissionError, setScoreSubmissionError] = useState(false);
+  const [scoreSubmissionMessage, setScoreSubmissionMessage] = useState('');
+  const [problematicScore, setProblematicScore] = useState([]);
 
 
   const isStepTwoBlocked = isValidEmail == false || email.length == 0 || creatorKey == '' || gameName == '';
@@ -47,6 +45,7 @@ const SubmitGameModal: React.FC<InputFormProps> = ({ closeModal }) => {
 
   const handleJSONUpload = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
+      setIsValidFile(true);
       setSelectedFile(e.target.files[0]);
     }
   };
@@ -78,8 +77,7 @@ const SubmitGameModal: React.FC<InputFormProps> = ({ closeModal }) => {
     setStep(3);
   }
 
-  const handleFormSubmission = async () => {
-    setStep(4);
+  const handleScoreSubmission = async () => {
     if (selectedFile) {
       try {
         const reader = new FileReader();
@@ -92,7 +90,8 @@ const SubmitGameModal: React.FC<InputFormProps> = ({ closeModal }) => {
               setIsValidFile(true);
             } catch (e) {
               setIsValidFile(false);
-              alert("Selected file is not a valid JSON file");
+              setSelectedFile(null);
+              alert("Selected file is not a valid JSON file. Double check your syntax to ensure there are no violations.");
               return;
             }
           }
@@ -108,9 +107,14 @@ const SubmitGameModal: React.FC<InputFormProps> = ({ closeModal }) => {
           })
 
           if (!addScoresResponse.ok) {
-            console.error(addScoresResponse);
-            return;
+            setScoreSubmissionError(true);
+            const error = await addScoresResponse.json();
+            setScoreSubmissionMessage(error.message);
+            setProblematicScore(error.problematicScore);
+            setSelectedFile(null);
           }
+
+          setStep(5);
         }
       } catch (error) {
         console.log(error);
@@ -159,21 +163,21 @@ const SubmitGameModal: React.FC<InputFormProps> = ({ closeModal }) => {
                   <div className="h-5/6 w-full">
                     {gameCreationError ? (
                       <div className="flex flex-col items-center w-full h-full">
-                        <div className="flex flex-col items-center w-full h-1/3">
+                        <div className="flex flex-col items-center w-full h-2/5">
                           <XCircleIcon className="h-16 w-16 stroke-red-500" />
                           <h1 className="text-lg">Game Creation Failed</h1>
                         </div>
-                        <div className="w-full h-2/3 overflow-y-auto border">
+                        <div className="w-full h-3/5 overflow-y-auto border">
                           <p><strong className="text-red-500">Error:</strong> {gameCreationMessage}</p>
                         </div>
                       </div>
                     ) : (
                       <div className="flex flex-col items-center w-full h-full">
-                        <div className="flex flex-col items-center w-full h-1/3">
+                        <div className="flex flex-col items-center w-full h-2/5">
                           <CheckCircleIcon className="h-16 w-16 stroke-green-500" />
                           <h1 className="text-lg">Game Creation Successful!</h1>
                         </div>
-                        <div className="w-full h-2/3 overflow-y-auto border">
+                        <div className="w-full h-3/5 overflow-y-auto border">
                           <p>
                             Click continue to add your list of scores or close this window and programmatically add scores one-by-one via our API.
                           </p>
@@ -182,9 +186,71 @@ const SubmitGameModal: React.FC<InputFormProps> = ({ closeModal }) => {
                     )}
                   </div>
                 ) : step == 4 ? (
-                  <div>
-                    <label htmlFor="scores-input" className="text-sm font-bold">Scores (JSON files only)</label>
-                    <input id="scores-input" onChange={handleJSONUpload} className={`block w-full py-1 text-sm border ${isValidFile ? "border-gray-300" : "border-red-500"} rounded-md cursor-pointer bg-white`} type="file" accept=".json" />
+                  <div className="h-5/6 w-full">
+                    <div className="flex flex-col h-full w-full justify-between">
+                      <div>
+                        <label htmlFor="scores-input" className="text-sm font-bold">Scores (JSON files only)</label>
+                        <input id="scores-input" onChange={handleJSONUpload} className={`block w-full py-1 text-sm border ${isValidFile ? "border-gray-300" : "border-red-500"} rounded-md cursor-pointer bg-white`} type="file" accept=".json" />
+                      </div>
+                      <div>
+                        <strong className="text-sm">Please Note:</strong>
+                        <p className="text-sm">If you are trying to correct a file error from the next step, reset the file selector by choosing a different file from your system and then immediately change it back to the desired file before submitting.</p>
+                      </div>
+                    </div>
+                  </div>
+                ) : step == 5 ? (
+                  <div className="h-5/6 w-full">
+                    {scoreSubmissionError ? (
+                      <div className="flex flex-col items-center w-full h-full">
+                        <div className="flex flex-col items-center w-full h-2/5">
+                          <XCircleIcon className="h-16 w-16 stroke-red-500" />
+                          <h1 className="text-lg">Score Submission Failed</h1>
+                        </div>
+                        <div className="w-full h-3/5 overflow-y-auto border">
+                          <p><strong className="text-red-500">Error:</strong> {scoreSubmissionMessage}</p>
+                          <strong className="text-red-500">Problem:</strong>
+                          <div>
+                            {"{"}
+                            <div className="ml-4">
+                              {Object.keys(problematicScore).map((key: string) => {
+                                const value = (problematicScore as { [key: string]: any })[key];
+                                if (typeof value === 'object' && value !== null) {
+                                  // If the value is an object, render its properties as well.
+                                  return (
+                                    <div key={key}>
+                                      <span className="text-blue-600">{key}</span>: {"{"}
+                                      <div className="ml-4">
+                                        {Object.keys(value).map(subKey => (
+                                          <div key={subKey}>
+                                            <span className="text-blue-600">{subKey}</span>:{" "}
+                                            <span className="text-orange-500">{JSON.stringify(value[subKey])}</span>
+                                          </div>
+                                        ))}
+                                      </div>
+                                      {"}"}
+                                    </div>
+                                  );
+                                }
+
+                                // If the value is not an object, just render it directly.
+                                return (
+                                  <div key={key}>
+                                    <span className="text-blue-600">{key}</span>:{" "}
+                                    <span className="text-orange-500">{JSON.stringify(value)}</span>
+                                  </div>
+                                );
+                              })}
+                            </div>
+                            {"}"}
+                          </div>
+                        </div>
+                      </div>
+                    ) : (
+                      <div className="flex flex-col items-center w-full h-full">
+                        <CheckCircleIcon className="h-16 w-16 stroke-green-500" />
+                        <h1 className="text-lg">Score Submission Successful!</h1>
+                      </div>
+                    )}
                   </div>
                 ) : null }
               </div>
@@ -221,12 +287,21 @@ const SubmitGameModal: React.FC<InputFormProps> = ({ closeModal }) => {
                     {gameCreationError ? (
                       <div>
                         {gameCreationStatus == 409 ? (
-                          <button
-                            onClick={() => setStep(4)}
-                            className="bg-indigo-500 text-white font-bold uppercase text-sm px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-                          >
-                            Continue
-                          </button>
+                          <div>
+                            <button
+                              onClick={() => setStep(1)}
+                              className="bg-green-500 text-white font-bold uppercase text-sm px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 disabled:bg-gray-400"
+                            >
+                              Back
+                            </button>
+                            <button
+                              onClick={() => setStep(4)}
+                              className="bg-indigo-500 text-white font-bold uppercase text-sm px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 disabled:bg-gray-400"
+                              disabled={!allFieldsFilled}
+                            >
+                              Continue
+                            </button>
+                          </div>
                         ) : (
                           <button
                             onClick={() => setStep(2)}
@@ -263,13 +338,20 @@ const SubmitGameModal: React.FC<InputFormProps> = ({ closeModal }) => {
                       Back
                     </button>
                     <button
-                      onClick={handleFormSubmission}
+                      onClick={handleScoreSubmission}
                       className="bg-indigo-500 text-white font-bold uppercase text-sm px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 disabled:bg-gray-400"
                       disabled={isSubmissionBlocked}
                     >
                       Submit
                     </button>
                   </div>
+                ) : step == 5 ? (
+                  <button
+                    onClick={() => setStep(4)}
+                    className="bg-green-500 text-white font-bold uppercase text-sm px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 disabled:bg-gray-400"
+                  >
+                    Back
+                  </button>
                 ) : null}
               </div>
             </div>
