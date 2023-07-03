ee51490aa5e551347726a2da2f3dc6fca811bcf1
dmrudy17 <dmr136@zips.uakron.edu>
2023-07-03 17:41:46 -0400

    chore: edit button and link styles

--- vis-portal/src/pages/index.tsx
+++ vis-portal/src/pages/index.tsx
@@ -20,8 +20,8 @@ export default function Home() {
           <div className="grid grid-cols-3">
             <h1 className="m-auto text-center text-3xl col-start-2">VisPortal</h1>
             <div className="w-full flex space-x-2 justify-end pr-1">
-              <a href="/apiDocs" className="mr-1 mt-1 py-2 px-4 bg-white rounded hover:underline">Documentation</a>
-              <button className="mr-1 mt-1 py-2 px-4 bg-white rounded hover:underline" onClick={() => setShowTokenRequestModal(true)}>Request Creator Key</button>
+              <a href="/apiDocs" className="mr-1 mt-1 py-2 px-4 bg-white rounded hover:text-indigo-500">Documentation</a>
+              <button className="mr-1 mt-1 py-2 px-4 bg-white rounded hover:text-indigo-500" onClick={() => setShowTokenRequestModal(true)}>Request Creator Key</button>
               <button className="mr-1 mt-1 py-2 px-4 bg-indigo-500 rounded text-white hover:bg-indigo-600" onClick={() => setShowSubmitGameModal(true)}>Submit New Game</button>
             </div>
           </div>
