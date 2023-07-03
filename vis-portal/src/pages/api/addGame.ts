0fd52a4ea1fb1e7619a1b6e19c98dc0ee2369fb3
dmrudy17 <dmr136@zips.uakron.edu>
2023-07-03 15:31:38 -0400

    feat: remove boolean as a supported type and adjust messaging

--- vis-portal/src/pages/api/addGame.ts
+++ vis-portal/src/pages/api/addGame.ts
@@ -29,10 +29,10 @@ export default async function handler(
 
     const scoreRequirements = game.score_requirements;
     if (Object.keys(scoreRequirements).length < 2) {
-      return res.status(400).json({ message: "Please provide at least two scoring parameters for visualization" });
+      return res.status(400).json({ message: "Please provide at least two distinct scoring parameters for visualization" });
     }
     
-    const allowedTypes = ['string', 'number', 'boolean'];
+    const allowedTypes = ['string', 'number'];
     for (let key of Object.keys(scoreRequirements)) {
       const type = scoreRequirements[key];
       if (typeof type !== "string" || !allowedTypes.includes(type)) {
