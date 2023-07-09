3bbc03bd8268e71eccd2024f110658f94c076a28
dmrudy17 <dmr136@zips.uakron.edu>
2023-07-09 00:44:06 -0400

    feat: add loading indicator throughout game submission

--- vis-portal/src/pages/components/SubmitGameModal.tsx
+++ vis-portal/src/pages/components/SubmitGameModal.tsx
@@ -24,9 +24,8 @@ const SubmitGameModal: React.FC<InputFormProps> = ({ closeModal }) => {
   const [isValidFile, setIsValidFile] = useState(true);
   const [creatorKey, setCreatorKey] = useState('');
   const [gameName, setGameName] = useState('');
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(4);
   const [allFieldsFilled, setAllFieldsFilled] = useState(false);
-  // const [requirements, setRequirements] = useState<{ [key: string]: string }>({});
   const [gameCreationError, setGameCreationError] = useState(false);
   const [gameCreationMessage, setGameCreationMessage] = useState('');
   const [gameCreationStatus, setGameCreationStatus] = useState(0);
@@ -37,6 +36,8 @@ const SubmitGameModal: React.FC<InputFormProps> = ({ closeModal }) => {
     { field: '', type: 'number' },
     { field: '', type: 'number' }
   ]);
+  const [gameLoading, setGameLoading] = useState(false);
+  const [scoresLoading, setScoresLoading] = useState(false);
 
   const isStepTwoBlocked = isValidEmail == false || email.length == 0 || creatorKey == '' || gameName == '';
   const isSubmissionBlocked = !selectedFile || !isValidFile
@@ -73,6 +74,7 @@ const SubmitGameModal: React.FC<InputFormProps> = ({ closeModal }) => {
   };
 
   const handleGameCreation = async () => {
+    setGameLoading(true);
     setGameCreationError(false);
     setGameCreationMessage('');
     
@@ -103,15 +105,18 @@ const SubmitGameModal: React.FC<InputFormProps> = ({ closeModal }) => {
       }
 
       setStep(3);
+      setGameLoading(false);
     } catch (error) {
       console.error(error);
       setGameCreationError(true);
       setGameCreationMessage("An unexpected error occurred. Please try again later.");
+      setGameLoading(false);
     }
   }
 
   const handleScoreSubmission = async () => {
     if (selectedFile) {
+      setScoresLoading(true);
       setScoreSubmissionError(false);
       setScoreSubmissionMessage('');
       setProblematicScore([]);
@@ -151,12 +156,14 @@ const SubmitGameModal: React.FC<InputFormProps> = ({ closeModal }) => {
           }
 
           setStep(5);
+          setScoresLoading(false);
         }
       } catch (error) {
         console.error(error);
         setScoreSubmissionError(true);
         setScoreSubmissionMessage("An unexpected error occurred. Please try again later");
         setSelectedFile(null);
+        setScoresLoading(false);
       }
     }
   }
@@ -310,20 +317,35 @@ const SubmitGameModal: React.FC<InputFormProps> = ({ closeModal }) => {
                     Continue
                   </button>
                 ) : step == 2 ? (
-                  <div>
+                  <div className="flex">
                     <button
-                      onClick={() => setStep(1)}
+                      onClick={() => {
+                          setStep(1);
+                          setGameLoading(true);
+                        }
+                      }
                       className="bg-green-500 text-white font-bold uppercase text-sm px-6 py-2 rounded shadow hover:bg-green-600 outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 disabled:bg-gray-400"
                     >
                       Back
                     </button>
-                    <button
-                      onClick={handleGameCreation}
-                      className="bg-indigo-500 text-white font-bold uppercase text-sm px-6 py-2 rounded shadow hover:bg-indigo-600 outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 disabled:bg-gray-400"
-                      disabled={!allFieldsFilled}
-                    >
-                      Continue
-                    </button>
+                    {!gameLoading ? (
+                      <button
+                        onClick={handleGameCreation}
+                        className="bg-indigo-500 text-white flex justify-center w-28 font-bold uppercase text-sm px-6 py-2 rounded shadow hover:bg-indigo-600 outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 disabled:bg-gray-400"
+                        disabled={!allFieldsFilled}
+                      >
+                        Continue
+                      </button>
+                    ) : (
+                      <button
+                        className="bg-gray-400 flex justify-center w-28 px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                      >
+                        <svg aria-hidden="true" className="w-5 h-5 text-gray-200 animate-spin fill-indigo-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                        </svg>
+                      </button>
+                    )}
                   </div>
                 ) : step == 3 ? (
                   <div>
@@ -373,20 +395,31 @@ const SubmitGameModal: React.FC<InputFormProps> = ({ closeModal }) => {
                     )}
                   </div>
                 ) : step == 4 ? (
-                  <div>
+                  <div className="flex">
                     <button
                       onClick={() => setStep(3)}
                       className="bg-green-500 text-white font-bold uppercase text-sm px-6 py-2 rounded shadow hover:bg-green-600 outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 disabled:bg-gray-400"
                     >
                       Back
                     </button>
-                    <button
-                      onClick={handleScoreSubmission}
-                      className="bg-indigo-500 text-white font-bold uppercase text-sm px-6 py-2 rounded shadow hover:bg-indigo-600 outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 disabled:bg-gray-400"
-                      disabled={isSubmissionBlocked}
-                    >
-                      Submit
-                    </button>
+                    {!scoresLoading ? (
+                      <button
+                        onClick={handleScoreSubmission}
+                        className="bg-indigo-500 text-white flex justify-center w-24 font-bold uppercase text-sm px-6 py-2 rounded shadow hover:bg-indigo-600 outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 disabled:bg-gray-400"
+                        disabled={isSubmissionBlocked}
+                      >
+                        Submit
+                      </button>
+                    ) : (
+                      <button
+                        className="bg-gray-400 flex justify-center w-24 px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                      >
+                        <svg aria-hidden="true" className="w-5 h-5 text-gray-200 animate-spin fill-indigo-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                        </svg>
+                      </button>
+                    )}
                   </div>
                 ) : step == 5 && scoreSubmissionError ? (
                   <button
